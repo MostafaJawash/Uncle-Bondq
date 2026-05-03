b7c126706ff0a3cdcfac93c9f57f4dcd141552ce
Mostafa jawash <166821013+MostafaJawash@users.noreply.github.com>
2026-05-03 14:56:52 +0300

    Change base path in Vite configurationة

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ChocoLux/',
+  base: '/Uncle-Bondq/',
   plugins: [react(), tailwindcss()],
 })
